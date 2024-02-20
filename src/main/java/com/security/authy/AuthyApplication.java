664147203f34ca82997996cd93c0cdec1e7e6ad1
Paulo <zhonions1234@gmail.com>
2024-02-20 12:52:08 +0000

    project setup

---
+++ src/main/java/com/security/authy/AuthyApplication.java
@@ -0,0 +1,13 @@
+package com.security.authy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthyApplication.class, args);
+	}
+
+}
